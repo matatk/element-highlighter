b402af89c698773fe69c9965318b153a00e99d02
Matthew Tylee Atkinson <matatk@gmail.com>
2021-12-04 13:09:47 +0000

    feat: Landmarks for landmark matches
    If a match already is a landmark region, this wraps it to create the
    region for indicating the match.

--- extension/content.js
+++ extension/content.js
@@ -4,31 +4,39 @@ const settings = {
 	'selector': null,
 	'outline': '4px solid yellow'
 }
-const LANDMARK_FLAG = 'data-highlight-selector-landmark'
+const LANDMARK = 'data-highlight-selector-landmark'
 const highlighted = new Set([])
 const originalInlineOutlines = {}
 let highlightOutline = null
 let counter = 0
 
+function makeElementIntoLandmark(element, asWrapper) {
+	element.setAttribute('role', 'region')
+	element.setAttribute('aria-roledescription', 'Highlight')
+	element.setAttribute('aria-label', ++counter)
+	element.setAttribute(LANDMARK, asWrapper ? 'wrapper' : '')
+}
+
 function highlight(elements) {
 	for (const element of elements) {
-		if (!highlighted.has(element)) {
-			originalInlineOutlines[element] = element.style.outline
-			element.style.outline = highlightOutline
-			highlighted.add(element)
+		if (highlighted.has(element)) continue
 
-			// Make it a landmark region
-			if (element.getAttribute('role') ||
-				element.getAttribute('aria-labelledby') ||
-				element.getAttribute('aria-label')) {
-				// TODO
-			} else {
-				element.setAttribute('role', 'region')
-				element.setAttribute('aria-roledescription', 'Highlight')
-				element.setAttribute('aria-label', ++counter)
-				element.setAttribute(LANDMARK_FLAG, '')
-			}
+		originalInlineOutlines[element] = element.style.outline
+		element.style.outline = highlightOutline
+
+		// Make it a landmark region
+		if (element.getAttribute('role') ||
+			element.getAttribute('aria-labelledby') ||
+			element.getAttribute('aria-label')) {
+			const wrapper = document.createElement('DIV')
+			makeElementIntoLandmark(wrapper, true)
+			element.parentElement.insertBefore(wrapper, element)
+			wrapper.appendChild(element)
+		} else {
+			makeElementIntoLandmark(element, false)
 		}
+
+		highlighted.add(element)
 	}
 }
 
@@ -40,22 +48,25 @@ function updateOutlines() {
 
 function removeHighlightsExceptFor(matches = new Set()) {
 	for (const element of highlighted) {
-		if (!matches.has(element)) {
-			element.style.outline = originalInlineOutlines[element] ?? ''
-			if (element.getAttribute('style') === '') {
-				element.removeAttribute('style')
-			}
-			delete originalInlineOutlines[element]
-			highlighted.delete(element)
+		if (matches.has(element)) continue
 
-			// Remove the landmark region if present
-			if (element.hasAttribute(LANDMARK_FLAG)) {
-				element.removeAttribute('role')
-				element.removeAttribute('aria-roledescription')
-				element.removeAttribute('aria-label')
-				element.removeAttribute(LANDMARK_FLAG)
-			}
+		element.style.outline = originalInlineOutlines[element] ?? ''
+		if (element.getAttribute('style') === '') {
+			element.removeAttribute('style')
 		}
+		delete originalInlineOutlines[element]
+
+		// Remove the landmark region if present
+		if (element.hasAttribute(LANDMARK)) {
+			element.removeAttribute('role')
+			element.removeAttribute('aria-roledescription')
+			element.removeAttribute('aria-label')
+			element.removeAttribute(LANDMARK)
+		} else if (element.parentElement.getAttribute(LANDMARK) === 'wrapper') {
+			element.parentElement.replaceWith(element)
+		}
+
+		highlighted.delete(element)
 	}
 }
 
