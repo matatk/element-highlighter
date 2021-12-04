ac6ec4d041112416a2db00dec54479a1834f3be3
Matthew Tylee Atkinson <matatk@gmail.com>
2021-12-04 15:57:48 +0000

    fix: Remove unused variable

--- extension/background.js
+++ extension/background.js
@@ -1,10 +1,8 @@
 'use strict'
 chrome.commands.onCommand.addListener(name => {
-	chrome.storage.sync.get('selector', items => {
-		if (name === 'update-highlights') {
-			chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-				chrome.tabs.sendMessage(tabs[0].id, { name })
-			})
-		}
-	})
+	if (name === 'update-highlights') {
+		chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+			chrome.tabs.sendMessage(tabs[0].id, { name })
+		})
+	}
 })
