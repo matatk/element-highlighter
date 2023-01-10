matatk/element-highlighter

commit 22631a2616d09d6715a6bc35f4c96ffcffdefa68

fix: Don't highlight <body> or above

extension/content.js
@@ -182,6 +182,16 @@ function selectAndhighlight(incrementRunCounter, removeAllHighlights) {
 function evaluatePathAndSetValidity() {
 	const nodeList = []
 	let result = null
+
+	function addNoBigNodes(node) {
+		if (node === document ||
+			node === document.documentElement ||
+			node === document.body) {
+			return
+		}
+		nodeList.push(node)
+	}
+
 	try {
 		result = document.evaluate(
 			gCachedLocator, document, null, XPathResult.ANY_TYPE, null)
@@ -194,16 +204,17 @@ function evaluatePathAndSetValidity() {
 				case XPathResult.UNORDERED_NODE_ITERATOR_TYPE: {
 					let node = null
 					while (node = result.iterateNext()) {
-						nodeList.push(node)
+						addNoBigNodes(node)
 					}
 				}
 					break
 				case XPathResult.ANY_UNORDERED_NODE_TYPE:
 				case XPathResult.FIRST_ORDERED_NODE_TYPE:
-					nodeList.push(result.singleNodeValue)  // TODO: test
+					addNoBigNodes(result.singleNodeValue)  // TODO: test
 			}
 		}
 	}
+
 	return nodeList
 }
 

commit b896e593c034f9a6b702142a76e42521260fe483

fix: Handle empty results
22631a2 introduced a bug because only `undefined` is ignored when
passing the found elements to the highlight function. This also neatens
up the code.

extension/content.js
@@ -140,10 +140,11 @@ function selectAndhighlight(incrementRunCounter, removeAllHighlights) {
 	gValidLocator = true
 	gMatchCounter = 0
 	if (gState !== states.manual) gState = states.notObserving
-	let foundElements = null
+	const foundElements = new Set()
 
 	if (gCachedLocator) {
 		let nodeList = null
+
 		if (gCachedLocator.startsWith('/')) {
 			nodeList = evaluatePathAndSetValidity()
 		} else {
@@ -153,15 +154,19 @@ function selectAndhighlight(incrementRunCounter, removeAllHighlights) {
 				gValidLocator = false
 			}
 		}
+
 		if (gValidLocator) {
-			foundElements = new Set(Array.from(nodeList).filter(
-				element => !element.hasAttribute(LANDMARK_MARKER_ATTR)))
+			for (const match of nodeList) {
+				if (!match.hasAttribute(LANDMARK_MARKER_ATTR)) {
+					foundElements.add(match)
+				}
+			}
 			gMatchCounter = foundElements.size
 			if (incrementRunCounter) gRunCounter++
 		}
 	}
 
-	if (!gCachedLocator || !gValidLocator || foundElements) {
+	if (!gCachedLocator || !gValidLocator || foundElements.size === 0) {
 		stopObserving()
 		if (removeAllHighlights) {
 			removeHighlightsExceptFor()  // when changing landmarks seting